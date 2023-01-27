279d40e7922f9668694b625d92ef5cfe3accfb07
aon <21188659+aon@users.noreply.github.com>
2023-01-26 21:38:11 -0300

    Add db connection to subscribe endpoint and update tests

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-use zero2prod::{startup::run, configuration::get_configuration};
+use sqlx::PgPool;
+use zero2prod::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let listener = std::net::TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connecto to Postgres.");
+    let listener =
+        std::net::TcpListener::bind(format!("127.0.0.1:{}", configuration.application_port))?;
+    run(listener, connection_pool)?.await
 }
